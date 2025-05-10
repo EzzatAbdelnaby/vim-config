530bd5b084376a6bba81c914c37a863f9bc0ee5b
EzzatAbdelnaby <99536786+EzzatDiaa@users.noreply.github.com>
2025-05-10 08:56:12 +0300

    Feat:TUMX for mulit projects

--- init.lua
+++ init.lua
@@ -7,3 +7,6 @@ require("config.autocmds-typescript")
 
 -- Load custom autocmds for React
 require("config.autocmds-react")
+-- if vim.fn.argc() == 0 and #vim.api.nvim_list_tabpages() == 1 then
+--   require("workspace-tabs").setup()
+-- end
